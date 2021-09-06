354d0cea39293674f171f3b9374d3a3d90ff8d23
vidyanand786 <vidyanandkumar3573@gmail.com>
2021-09-06 18:09:27 +0530

    implement the doctor , appointment, patient enity

--- online_doctor_appointment/src/main/java/online/doctor/appointment/entities/Doctor.java
+++ online_doctor_appointment/src/main/java/online/doctor/appointment/entities/Doctor.java
@@ -1,5 +1,52 @@
 package online.doctor.appointment.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Doctor {
 
+	@Id
+	private String email;
+	
+	private String name;
+	private String category;
+	private  int  fee;
+	private String password;
+	private String mobno;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public int getFee() {
+		return fee;
+	}
+	public void setFee(int fee) {
+		this.fee = fee;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
 }
--- online_doctor_appointment/src/main/java/online/doctor/appointment/entities/Patient.java
+++ online_doctor_appointment/src/main/java/online/doctor/appointment/entities/Patient.java
@@ -1,5 +1,40 @@
 package online.doctor.appointment.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
 public class Patient {
 
+	private String email;
+	private String name;
+	private String password;
+	private String mobno;
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getMobno() {
+		return mobno;
+	}
+	public void setMobno(String mobno) {
+		this.mobno = mobno;
+	}
+	
+	
 }
